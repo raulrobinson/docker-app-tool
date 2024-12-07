7ca80ad050d1221d9bff7ebb673b972b8ef07969
rasys <rasysbox@hotmail.com>
2024-12-07 11:14:14 -0500

    enable stats containers

--- src/app/components/stats-modal.tsx
+++ src/app/components/stats-modal.tsx
@@ -1,8 +1,8 @@
 const StatsModal = ({
-                        show,
-                        stats,
-                        onClose,
-                    }: {
+    show,
+    stats,
+    onClose,
+}: {
     show: boolean;
     stats: Record<string, any> | string;
     onClose: () => void;
@@ -16,7 +16,7 @@ const StatsModal = ({
             className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center ${show ? "" : "hidden"}`}
             style={{ zIndex: 9999 }}
         >
-            <div className="bg-white text-gray-800 w-11/12 sm:w-10/12 md:w-full p-4 rounded-lg max-h-[100vh] overflow-auto">
+            <div className="bg-white text-gray-800 w-11/12 sm:w-10/12 md:w-1/2 p-4 rounded-lg max-h-[100vh] overflow-auto">
                 <div className="flex justify-between">
                     <h2 className="text-xl font-bold">Container Stats</h2>
                     <button onClick={onClose} className="text-red-500 text-4xl">
